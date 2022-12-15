67a1dc33bbcf59ac8b1691a07f5dc8a1fb3f7170
Sihle Mthembu <innocentsihle29@gmail.com>
2022-12-15 21:20:50 +0200

    Note sure where my entity went agin

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -4,5 +4,10 @@ namespace API.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> MyProperty { get; set; }
     }
 }
