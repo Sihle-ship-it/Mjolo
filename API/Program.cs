64d886673a5a90365078d74d5f55513295a8d093
Sihle Mthembu <innocentsihle29@gmail.com>
2022-12-18 11:20:23 +0200

    Added JWT Token

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using API;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,13 +14,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-//app.UseCors(builder => builder.WithOrigins("htpp://localhost:4200").AllowAnyHeader().AllowAnyMethod());
+
 app.MapControllers();
 app.UseCorsMiddleware();
 
