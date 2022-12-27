bdb7a958e629fe827306ca02ff8444032138b105
Sihle Mthembu <innocentsihle29@gmail.com>
2022-12-27 07:07:31 +0200

    Working on route gaurds

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,14 +4,21 @@ import { ListsComponent } from './Components/lists/lists.component';
 import { MemberDetailComponent } from './Components/members/member-detail/member-detail.component';
 import { MemberListComponent } from './Components/members/member-list/member-list.component';
 import { MessagesComponent } from './Components/messages/messages.component';
+import { AuthGuard } from './Guards/auth.guard';
 import { HomeComponent } from './Pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'members', component: MemberListComponent },
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent},
+    ]
+  },
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
